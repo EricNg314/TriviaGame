49f23fc1b5999a31cd02eef4794c5104d224f752
Okens <Ng.Eric314@gmail.com>
2018-05-02 23:13:13 -0700

    Separating scoreboard from main display to separate view. Creating a temporary array to hold questions.

---
+++ assets/javascript/app.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+
+var questionArr = [{
+    question: "The answer is 1",
+    choices: ["1", "2", "3", "4"],
+    answer: 0
+}, {
+    question: "The answer is 2",
+    choices: ["1", "2", "3", "4"],
+    answer: 1
+}, {
+    question: "The answer is 3",
+    choices: ["1", "2", "3", "4"],
+    answer: 2
+}, {
+    question: "The answer is 4",
+    choices: ["1", "2", "3", "4"],
+    answer: 3
+}, {
+    question: "The answer is 1",
+    choices: ["1", "2", "3", "4"],
+    answer: 0
+}, {
+    question: "The answer is 2",
+    choices: ["1", "2", "3", "4"],
+    answer: 1
+}, {
+    question: "The answer is 3",
+    choices: ["1", "2", "3", "4"],
+    answer: 2
+}, {
+    question: "The answer is 4",
+    choices: ["1", "2", "3", "4"],
+    answer: 3
+},]
+
+
+
+
+
+
+
+
+
+
+
+
+});
