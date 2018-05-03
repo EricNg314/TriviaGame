f0ee949671cc7c35aa4faecb6262ae640a3f8a92
Okens <Ng.Eric314@gmail.com>
2018-05-03 00:09:39 -0700

    Adding pseudo code mapping game process into app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,105 @@
+//========== Initialization Part 1 ===================================================//
+
+//Create array for question objects.
+//Create variable object to hold if answer is correct/incorrect/outOfTIme
+
+
+//========= Initialization Part 2 ====================================================//
+
+//Initial page should be cover with instructions.
+//If start is clicked, then reset game etc.
+
+//output to question page.
+//If start is clicked, then apply reset game etc.
+//Apply class "d-none" to id "jumbotronID".
+//Remove class "d-none" from id "gameBoxID".
+
+
+//========== Clicking Event to Question ===================================================//
+
+//Start Timer
+//Display question, image, choices and timer.
+
+
+//========== Clicking Event to Question(Choice Clicked) ===================================================//
+
+//Timer pause.
+//Create a delay timer for 4seconds to move on after the correct/incorrect.
+//If correct then add 1 to wins and state "correct"
+//If incorrect then add 1 to loss, state "incorrect", and what was correct answer.
+//If timer is out, then add 1 to unanswered.
+
+//Move to next question.
+//Timer Reset
+//Check if last question, if true then go to "Scoreboard"
+////Apply class "d-none" to id "gameBoxID".
+////Remove class "d-none" from id "scoreBoxID".
+
+//ELSE Go to next question and back to "Clicking Event to Question"
+
+
+//========== Event to Scoreboard ===================================================//  
+
+//Show number of correct (wins)
+//Show number of incorrect (loss)
+//Show number of unanswered
+
+//Click to restart.
+//Remove class "d-none" from id "gameBoxID".
+//Apply class "d-none" to id "scoreBoxID".
+
+
+//========================================================================================//
+
+
+//create a function to start game (or resetGame).
+//create a function to output display for questions, answers, and scores.
+
+//create a function to start timer.
+//create a function to stop timer.
+//create a function to reset timer.
+
+
 $(document).ready(function () {
 
 var questionArr = [{
     question: "The answer is 1",
+    image: "",
     choices: ["1", "2", "3", "4"],
     answer: 0
 }, {
     question: "The answer is 2",
+    image: "",
     choices: ["1", "2", "3", "4"],
     answer: 1
 }, {
     question: "The answer is 3",
+    image: "",
     choices: ["1", "2", "3", "4"],
     answer: 2
 }, {
     question: "The answer is 4",
+    image: "",
     choices: ["1", "2", "3", "4"],
     answer: 3
 }, {
     question: "The answer is 1",
+    image: "",
     choices: ["1", "2", "3", "4"],
     answer: 0
 }, {
     question: "The answer is 2",
+    image: "",
     choices: ["1", "2", "3", "4"],
     answer: 1
 }, {
     question: "The answer is 3",
+    image: "",
     choices: ["1", "2", "3", "4"],
     answer: 2
 }, {
     question: "The answer is 4",
+    image: "",
     choices: ["1", "2", "3", "4"],
     answer: 3
 },]
